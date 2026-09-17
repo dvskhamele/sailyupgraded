d41305b8e4eb639321b1b83c3bd8d84cb7a9fcb9
Manas-soni-07 <manassoninit07@gmail.com>
2026-09-17 21:06:06 +0530

    people fix done3

--- actions/crm/people/get-people.ts
+++ actions/crm/people/get-people.ts
@@ -402,7 +402,7 @@ export async function getUnifiedPeople(
     });
     try {
       apolloResponse = await fetch(targetUrl, {
-        signal: AbortSignal.timeout(60000),
+        signal: AbortSignal.timeout(1200000),
         cache: "no-store",
         headers: { Accept: "application/json" },
       });
